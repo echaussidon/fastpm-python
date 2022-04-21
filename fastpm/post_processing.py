8e97161efb5b49a0fd44290e8f897e820d34daa7
echaussidon <edmondchau@sfr.fr>
2022-04-21 02:43:53 -0700

    correct logger

--- fastpm/post_processing.py
+++ fastpm/post_processing.py
@@ -204,7 +204,7 @@ if __name__ == '__main__':
                         boxsize=attrs['boxsize'][0], boxcenter=attrs['boxsize'][0] // 2, resampler='tsc', interlacing=2, los='x',
                         position_type='pos', mpicomm=mpicomm).poles.save(os.path.join(sim, f'particle-subsamp-power-{aout}.npy'))
         mem_monitor()
-        logger_info(logger, f"Subsampling (from {particles.csize} to approx. {particles.csize * args.subsampling_nbr}) done in {MPI.Wtime() - start:2.2f} s.", rank)
+        logger_info(logger, f"Subsampling (from {particles.csize:2.2e} to approx. {particles.csize / args.subsampling_nbr:2.2e}) done in {MPI.Wtime() - start:2.2f} s.", rank)
 
     mem_monitor.stop_monitoring()
     mpicomm.Barrier()
