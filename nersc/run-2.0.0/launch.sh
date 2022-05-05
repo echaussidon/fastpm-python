e75b4b69f85ddb165e132913ab8527a7a64a60ae
echaussidon <edmondchau@sfr.fr>
2022-05-05 10:58:40 -0700

    update for fof

--- nersc/run-2.0.0/launch.sh
+++ nersc/run-2.0.0/launch.sh
@@ -3,7 +3,7 @@
 
 # launch fastpm then hall the post_processing treatment (halos finder + subsampler ect)
 
-sim_name='run-knl-3-fnl-0'
+sim_name='run-knl-3-fnl-TODO'
 aout_list=("0.2857" "0.3076" "0.3333" "0.3636" "0.4000")
 
 fastpm=$(sbatch --parsable fastpm.job $sim_name)
@@ -17,5 +17,5 @@ do
 
     subsample=$(sbatch --job-name subsampling-$aout --parsable --dependency=afterok:$halos subsample.job $sim_name $aout)
     echo "    * subsample: "$subsample
-    echo 
+    echo
 done
